1d6e521d92d941875331a64384ba7580ffef3f3d
Alexandr <alex@webgoal.ru>
2023-09-27 01:15:13 +0300

    Update

--- lib/means_transport/grount/grounttransport.h
+++ lib/means_transport/grount/grounttransport.h
@@ -8,6 +8,8 @@ public:
     GrountTransport(size_t dist);
     virtual ~GrountTransport();
 
+    double GetTime() override;
+
 protected:
     double TimeDoSleep{};  // Время движения до отдыха
     double TimeSleep[3]{}; // Длительность отдыха
